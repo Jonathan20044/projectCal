92d63d30c6f71424256b3e9872f9c20e4d00f2f2
jonathan20044 <116660564+Jonathan20044@users.noreply.github.com>
2026-04-21 20:34:43 -0600

    Update pacman.js

--- js/pacman.js
+++ js/pacman.js
@@ -1909,8 +1909,8 @@ function handleTouchStart(evt) {
 }
 
 function handleTouchMove(evt) {
-  if (xDown === null || yDown === null) return;
   evt.preventDefault();
+  if (xDown === null || yDown === null) return;
 
   var xUp = evt.touches[0].clientX;
   var yUp = evt.touches[0].clientY;
@@ -1927,12 +1927,10 @@ function handleTouchMove(evt) {
     keyCode = yDiff > 0 ? 38 : 40; // up : down
   }
 
-  // Dispatch a native KeyboardEvent so it works without jQuery
-  var e = new KeyboardEvent("keydown", {
-    keyCode: keyCode,
-    which: keyCode,
-    bubbles: true,
-  });
+  // Dispatch a native event with cross-browser keyCode polyfill (especially for iOS Safari)
+  var e = new Event("keydown", { bubbles: true, cancelable: true });
+  Object.defineProperty(e, "keyCode", { get: function() { return keyCode; } });
+  Object.defineProperty(e, "which", { get: function() { return keyCode; } });
   document.dispatchEvent(e);
 
   // Reset so the next swipe starts fresh
